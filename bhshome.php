c0add2e2c21d621e1877e525abce9fb32b3e24d3
AGUILAR GENESIS <655889@cart.ads>
2019-05-08 09:12:23 -0700

    initial commit

---
+++ bhshome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bullard Softball</title>
+</head>
+<body>
+    <nav>
+        <ul>
+        <li>Home</li>
+        <li>Schedules</li>
+        <li>About</li>
+        <li>Youth Program</li>
+        </ul>
+    </nav>
+    
+    
+    
+    
+    
+</body>
+</html>
