4555f2162d46ea0318d1cb3a04bb16f4d7b66997
PUENTE EMILY <570499@cart.ads>
2019-05-08 10:18:07 -0700

    schedule uncompleted all of march

---
+++ schedule.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Schedule Page</title>
+</head>
+<body>
+   <table style="width:100%">
+       <tr>
+           <th>Date</th>
+           <th>Opponent</th>
+           <th>Location</th>
+           <th>Time</th>
+       </tr>
+       <tr>
+           <th>2/12/19</th>
+           <th>Clovis North</th>
+           <th>Clovis North</th>
+           <th>3:30</th>
+       </tr>
+       <tr>
+           <th>2/15/19</th>
+           <th>Paso Robles (Varsity)</th>
+           <th>Clovis East</th>
+           <th>3:00</th>
+       </tr>
+       <tr>
+           <th>2/16/19</th>
+           <th>Mission Prep (Varsity)</th>
+           <th>Clovis East</th>
+           <th>9:00</th>
+       </tr>
+       <tr>
+           <th>2/16/19</th>
+           <th>Hanford (Varsity)</th>
+           <th>Clovis East</th>
+           <th>11:00</th>
+       </tr>
+       <tr>
+           <th>2/20/19</th>
+           <th>Liberty(Bakersfield) - (Varsity)</th>
+           <th>Liberty</th>
+           <th>2:00</th>
+       </tr>
+       <tr>
+           <th>2/22/19</th>
+           <th>Clovis (JV)</th>
+           <th>Clovis</th>
+           <th>3:00</th>
+       </tr>
+       <tr></tr>
+   </table>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
