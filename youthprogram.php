06cee74fb0652f4ce11417ab26b75fc75464814f
Genesis mena <genesismena202@gmail.com>
2019-05-11 12:06:23 -0700

    youth program page completed

--- youthprogram.php
+++ youthprogram.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Youth Program</title>
@@ -9,53 +10,71 @@
 
 
 </head>
+
 <body>
-   <div id="navigation">
-    <nav>
-       <h1>Bullard High Future Knights Softball</h1>
-        <ul>
-        <li><a href="bhshome.php">Home</a></li>
-        <li><a href="schedule.php">Schedules</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="youthprogram.php">Lil' Knights</a></li>
-        </ul>
-    </nav>
+    <div id="navigation">
+        <nav>
+            <h1>Bullard High Future Knights Softball</h1>
+            <ul>
+                <li><a href="bhshome.php">Home</a></li>
+                <li><a href="schedule.php">Schedules</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="youthprogram.php">Lil' Knights</a></li>
+            </ul>
+        </nav>
     </div>
-    <h2>Summer Skills Camps 2019</h2>
-        <h3>All Skills Camps</h3>
-        <p>
-        <strong>Lil' Knights(Ages 4-8)</strong> 8:30 am - 11:30 am June 10-13 100.00$
-        Summer softball camp. Come work with the Bullard softball team and coaching staff and improve your skills. <br> 
-        
-            <br><strong>Pitching/Catching(Ages 5-12)</strong> 12:00 pm - 1:30 pm June 10 - 12 50.00$ <br>
-        
-            <br> <strong>Left Handed Slappers Camp(Ages 5-12)</strong> 1:30 pm - 2:30 pm June 10-12 35.00$
-        
-        </p>
+    <div class="centeredtit">
+    <h1>Summer Skills Camps 2019</h1>
+        <h2>All Skills Camps</h2>
+        </div>
+    <div class="info">
+        <div>
+               <img src="images/action-active-activity-163365.jpg" alt="">
+            <p>
+                <strong>Lil' Knights(Ages 4-8)</strong> 8:30 am - 11:30 am June 10-13 100.00$
+                Summer softball camp. Come work with the Bullard softball team and coaching staff and improve your skills. <br>
+            </p>
+        </div>
+        <div>
+               <img src="images/action-athlete-ball-279004.jpg" alt="">
+            <p>
+                <br><strong>Pitching/Catching(Ages 5-12)</strong> 12:00 pm - 1:30 pm June 10 - 12 50.00$ <br>
+            </p>
+        </div>
+    </div>
+     <div id="bigbox">
+           <img src="images/action-athletes-ball-209852.jpg" alt="">
+            <p>
+                <br> <strong>Left Handed Slappers Camp(Ages 5-12)</strong> 1:30 pm - 2:30 pm June 10-12 35.00$
+            </p>
+       
+
         <ul>
             <li>Defense skill development - throwing and fielding fundamentals</li>
             <li>Infield and outfield position work</li>
             <li>Offense instruction - hitting and baserunning</li>
         </ul>
-    <p>Come get better with one of the best teams in the Valley!</p>
-    
-    <h2>Register Now</h2>
+        <p>Come get better with one of the best teams in the Valley!</p>
+    </div>
+<div class="centeredtit">
+    <h1>Register Now</h1>
+    </div>
     <hr>
     <form action="">
-        Name<input type="text">
-        Address<input type="text">
-        City<input type="text">
-        ZIP Code<input type="text">
-        Mom Name<input type="text">
-        Mom Cell<input type="text">
-        Dad Name<input type="text">
-        Dad Cell<input type="text">
-        E-mail<input type="email">
-        Emergency Contact<input type="text">
-        Date of Birth<input type="date">
-        Age<input type="number">
-        T-shirt Size<select name="tshirt" id="tshirt">
-           <option value="default" >-- Select --</option>
+        First Name <input type="text">    Last Name <input type="text"> <br>
+        Address <input type="text">
+        City <input type="text">
+        ZIP Code <input type="text"><br>
+        Mom Name <input type="text">
+        Mom Cell <input type="text"> <br>
+        Dad Name <input type="text">
+        Dad Cell <input type="text"> <br>
+        E-mail <input type="email"> <br>
+        Emergency Contact <input type="text"> <br>
+        Date of Birth <input type="date">
+        Age <input type="number" min="0"> <br>
+        T-shirt Size <select name="tshirt" id="tshirt">
+            <option value="default"> -- Select --</option>
             <option value="xs">Extra Small</option>
             <option value="s">Small</option>
             <option value="m">Medium</option>
@@ -64,4 +83,5 @@
         </select>
     </form>
 </body>
+
 </html>
