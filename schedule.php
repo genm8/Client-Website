e4b2359046bc0a5ff107ff4595ef65419f84393e
PUENTE EMILY <570499@cart.ads>
2019-05-14 08:40:50 -0700

    csv really close to being linked

--- schedule.php
+++ schedule.php
@@ -7,12 +7,23 @@
 </head>
 <body>
    <?php
-    $file = fopen("sched.csv", "r");
-    while( ! feof($file))
-    {
-        print_r()
-    }
-    fclose($file);
+        if (($csvfile = fopen("sched.csv", "x")) !== FALSE) {
+            while (($csvdata = fgetcsv($csvfile, 25, ",")) !== FALSE) {
+                $error= '';
+                $colcount = count($csvdata);
+                echo '<tr>';
+                if($colcount!=5) {
+                    $error = 'column count incorrect';
+                } else {
+                    if(!is_numeric($csvdata[0])) $error.='error';
+                    $date = date_parse($csvdata[3]);
+                    if (!($date["error_count"] == 0 && checkdate($date["month"], $date["day"], $date["year"]))) $error.='error';
+                    if(!is_numeric($csvdata[4])) $error.='error';
+                }
+                
+                echo '</tr>';
+            }
+        }
     ?>
     
 </body>
