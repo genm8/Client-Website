dc6281fc9a6dd9f45a44f4c42e492a7c30700dad
PUENTE EMILY <570499@cart.ads>
2019-05-14 09:38:44 -0700

    csv working with incorrect column count

--- schedule.php
+++ schedule.php
@@ -6,26 +6,49 @@
     <title>Schedule Page</title>
 </head>
 <body>
+  <table>
+      <tr>
+          <th>Date</th>
+          <th>Opponenet</th>
+          <th>Location</th>
+          <th>Time</th>
+      </tr>
+      
+      
+
    <?php
-        if (($csvfile = fopen("sched.csv", "x")) !== FALSE) {
-            while (($csvdata = fgetcsv($csvfile, 25, ",")) !== FALSE) {
+        if (($csvfile = fopen("sched.csv", "r")) !== FALSE) {
+            while (($csvdata = fgetcsv($csvfile, 0, ",")) !== FALSE) {
                 $error= '';
                 $colcount = count($csvdata);
-                echo '<tr>';
-                if($colcount!=5) {
+                //echo " count = $colcount <br>";
+                if($colcount!=4) {
                     $error = 'column count incorrect';
                 } else {
-                    if(!is_numeric($csvdata[0])) $error.='error';
-                    $date = date_parse($csvdata[3]);
-                    if (!($date["error_count"] == 0 && checkdate($date["month"], $date["day"], $date["year"]))) $error.='error';
-                    if(!is_numeric($csvdata[4])) $error.='error';
+                    //if(!is_numeric($csvdata[0])) $error.='not a date, ';
+                   //$date = date_parse($csvdata[0]);
+                    //if (!($date["error_count"] == 0 && checkdate($date["month"], $date["day"], $date["year"]))) $error.='date format, ';
+                    //if(!is_numeric($csvdata[3])) $error.='time, ';
+                }
+                if($error == '') {
+                    echo "<tr>
+                            <td>$csvdata[0]</td>
+                            <td>$csvdata[1]</td>
+                            <td>$csvdata[2]</td>
+                            <td>$csvdata[3]</td>
+                        </tr>      ";
+                } else {
+                    echo $error;
                 }
                 
-                echo '</tr>';
+                
             }
         }
     ?>
     
+    
+    
+    </table>  
 </body>
 </html>
 
