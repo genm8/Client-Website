65a8ead4f8eb7816ae65952acdd41eae0c63cf98
AGUILAR GENESIS <655889@cart.ads>
2019-05-16 10:17:35 -0700

    calendar added

--- bhshome.php
+++ bhshome.php
@@ -7,13 +7,17 @@
     <link rel="stylesheet" href="client-web.css">
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One|Bungee" rel="stylesheet">
+    
 
 
 </head>
 
 <body>
+   <div id="bigcontainer">
     <div class="header">
-        <a class="logo" href="bhshome.php">BSoftball</a>
+        <a class="logo" href="bhshome.php">
+            <img src="images/logo.PNG" alt="">
+        </a>
         <ul>
             <li><a href="bhshome.php">Home</a></li>
             <li><a href="schedule.php">Schedules</a></li>
@@ -21,8 +25,10 @@
             <li><a href="youthprogram.php">Lil' Knights</a></li>
         </ul>
     </div>
-
-
+</div>
+<div id="calendar">
+<iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=Ymhza25pZ2h0c29mdGJhbGxAZ21haWwuY29t&amp;src=ZmNkbmE2YXVldGFpaWJ1bG1yY3QzM2Vva2tAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23039BE5&amp;color=%234285F4&amp;showPrint=0&amp;title=Events%202019&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</div>
 </body>
 
 </html>
--- schedule.php
+++ schedule.php
@@ -76,13 +76,3 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, omnis cum dicta magni nulla nihil odio dolore, nobis maxime. Error assumenda, mollitia ea distinctio doloribus temporibus vitae corporis natus in!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus amet nobis hic optio officiis quos voluptatibus id, itaque et odit reiciendis vero eum delectus totam sunt, ipsam odio commodi eligendi.</p>
 </footer>
 </html>
-
-
-
-
-
-
-
-
-
-
